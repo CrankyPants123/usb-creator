7540243a4af4fb423d4da4e3ff9eb66950d680ab
andrew_dev <sxshodor@163.com>
2020-09-02 09:46:23 +0800

    Add files

---
+++ combostyle.h
@@ -0,0 +1,18 @@
+#ifndef COMBOSTYLE_H
+#define COMBOSTYLE_H
+
+//#include <QWidget>
+#include <QComboBox>
+#include <QBoxLayout>
+#include <QDebug>
+#include <QItemSelectionModel>
+
+class ComboStyle : public QComboBox
+{
+//    Q_OBJECT
+
+public:
+    ComboStyle();
+};
+
+#endif // COMBOSTYLE_H
